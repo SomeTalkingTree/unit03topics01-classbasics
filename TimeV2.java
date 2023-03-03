dd40cb7ee76424b86fc4370cf8f786015087cc46
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-03-03 12:14:03 -0600

    Dine

--- TimeV2.java
+++ TimeV2.java
@@ -1,5 +1,45 @@
 public class TimeV2 {
+    private int hour;
+    private int minute;        
+    private int second;
+    
+    public TimeV2(int Thehours,int Theminutes,int Theseconds){
+        hour =  Thehours;
+        minute = Theminutes;
+        second = Theseconds;
+    }
+    
+    public String toString(){
+        String fulltime ="";
+        int thehour = hour*3600;
+        int theminutes = minute*60;
+        int thesecond = thehour+theminutes+second;
+        fulltime+= "00:00:";
+        fulltime+= thesecond;
+            
+        return fulltime;
+    }
+    
+    
+    public int Thehours(){
+        return hour;
+    }
+    public int Theminutes(){
+        return minute;
+    }
+    
+    public int Theseconds(){
+        return second;
+    }
     public static void main(String[] args) {
-        
+        TimeV2 d1 = new TimeV2(5,15,19);
+        TimeV2 d2 = new TimeV2(6,2,26);
+        TimeV2 d3 = new TimeV2(4,9,15);
+
+        TimeV2[] allTimes = new TimeV2[] {d1,d2,d3};
+        for(TimeV2 Times:allTimes){
+            System.out.println(Times);
+        } 
     }
 }
+//javac TimeV2.java; java TimeV2
